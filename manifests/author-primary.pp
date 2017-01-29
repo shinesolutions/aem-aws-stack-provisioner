6e64a83771504aba6ac8f337c506e0db992de8fe
Cliffano Subagio <cliffano@gmail.com>
2017-01-30 03:20:07 +1100

    After starting AEM Author Primary and Publish, wait until login page is ready.

--- manifests/author-primary.pp
+++ manifests/author-primary.pp
@@ -5,6 +5,9 @@ class author_primary (
   service { 'aem-aem':
     ensure => 'running',
     enable => true,
+  } ->
+  aem_aem { 'Wait until login page is ready':
+    ensure  => login_page_is_ready,
   }
 
 }
--- manifests/publish.pp
+++ manifests/publish.pp
@@ -5,6 +5,9 @@ class publish (
   service { 'aem-aem':
     ensure => 'running',
     enable => true,
+  } ->
+  aem_aem { 'Wait until login page is ready':
+    ensure  => login_page_is_ready,
   }
 
 }
