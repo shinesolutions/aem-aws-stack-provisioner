be977f8653869261739b3a26fa34ba018f1beb58
Cliffano Subagio <cliffano@gmail.com>
2017-01-30 02:29:18 +1100

    Add explicit namespace to authorprimaryhost top-scope variable.

--- manifests/author-standby.pp
+++ manifests/author-standby.pp
@@ -4,7 +4,7 @@ class author_standby (
 
   class { 'aem_resources::author_standby_set_config':
     install_dir  => '/opt/aem/author/crx-quickstart/install',
-    primary_host => "${authorprimaryhost}",
+    primary_host => "${::authorprimaryhost}",
   } ->
   service { 'aem-aem':
     ensure => 'running',
