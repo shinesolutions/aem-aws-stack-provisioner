8625e4a3b40bd6c22027a157af8a401c7ba60ce2
Cliffano Subagio <cliffano@gmail.com>
2017-01-30 14:30:31 +1100

    Provision dispatcher config and graceful restart httpd.

--- manifests/author-dispatcher.pp
+++ manifests/author-dispatcher.pp
@@ -1,6 +1,19 @@
 class author_dispatcher (
-
+  $conf_dir,
+  $docroot_dir,
+  $author_port,
 ) {
+
+  class { 'aem_resources::author_dispatcher_set_config':
+    conf_dir    => "${conf_dir}",
+    docroot_dir => "${docroot_dir}",
+    author_host => 'someauthorhost',
+    author_port => "${author_port}",
+  } ->
+  exec { 'httpd -k graceful':
+    cwd  => '/tmp',
+    path => ['/sbin'],
+  }
 
 }
 
--- manifests/publish-dispatcher.pp
+++ manifests/publish-dispatcher.pp
@@ -1,6 +1,19 @@
 class publish_dispatcher (
-
+  $conf_dir,
+  $docroot_dir,
+  $publish_port,
 ) {
+
+  class { 'aem_resources::publish_dispatcher_set_config':
+    conf_dir     => "${conf_dir}",
+    docroot_dir  => "${docroot_dir}",
+    publish_host => 'somepublishhost',
+    publish_port => "${publish_port}",
+  } ->
+  exec { 'httpd -k graceful':
+    cwd  => '/tmp',
+    path => ['/sbin'],
+  }
 
 }
 
