2859e2e22e9b99ec80fb1093464cd5b89eaa8555
Cliffano Subagio <cliffano@gmail.com>
2017-02-09 14:19:09 +1100

    Parameterise author_standby class. Move puppet_conf_dir setting to common.

--- manifests/author-standby.pp
+++ manifests/author-standby.pp
@@ -1,16 +1,19 @@
 class author_standby (
-
+  $puppet_conf_dir,
+  $crx_quickstart_dir,
+  $author_protocol,
+  $author_port,
 ) {
 
   class { 'aem_resources::puppet_aem_resources_set_config':
-    conf_dir => '/etc/puppetlabs/puppet/',
-    protocol => 'http',
+    conf_dir => "${puppet_conf_dir}",
+    protocol => "${author_protocol}",
     host     => 'localhost',
-    port     => 4502,
+    port     => "${author_port}",
     debug    => true,
   } ->
   class { 'aem_resources::author_standby_set_config':
-    crx_quickstart_dir => '/opt/aem/author/crx-quickstart',
+    crx_quickstart_dir => "${crx_quickstart_dir}",
     primary_host       => "${::authorprimaryhost}",
   } ->
   service { 'aem-aem':
