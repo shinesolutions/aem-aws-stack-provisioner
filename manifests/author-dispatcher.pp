2c234234d645448adf116e2f2f379d02aa7cc79b
Cliffano Subagio <cliffano@gmail.com>
2017-02-15 21:44:18 +1100

    Provision AEM tools in author-dispatcher and publish-dispatcher.

--- manifests/author-dispatcher.pp
+++ manifests/author-dispatcher.pp
@@ -1,4 +1,5 @@
 class author_dispatcher (
+  $base_dir,
   $tmp_dir,
   $dispatcher_conf_dir,
   $httpd_conf_dir,
@@ -18,6 +19,48 @@ class author_dispatcher (
     path => ['/sbin'],
   }
 
+  # Set up AEM tools
+  file { "${base_dir}/aem-tools/":
+    ensure => directory,
+    mode   => '0775',
+    owner  => 'root',
+    group  => 'root',
+  } ->
+  file { "${base_dir}/aem-tools/deploy-artifact.sh":
+    ensure  => present,
+    content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/deploy-artifact.sh.epp", { 'base_dir' => "${base_dir}" }),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  } ->
+  file { "${base_dir}/aem-tools/deploy-artifacts.sh":
+    ensure  => present,
+    content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/deploy-artifacts.sh.epp", { 'base_dir' => "${base_dir}" }),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  } ->
+  file { "${base_dir}/aem-tools/export-backup.sh":
+    ensure  => present,
+    content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/export-backup.sh.epp", { 'base_dir' => "${base_dir}" }),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  } ->
+  file { "${base_dir}/aem-tools/import-backup.sh":
+    ensure  => present,
+    content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/import-backup.sh.epp", { 'base_dir' => "${base_dir}" }),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  } ->
+  file { "${base_dir}/aem-tools/promote-author-standby-to-primary.sh":
+    ensure  => present,
+    content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/promote-author-standby-to-primary.sh.epp", { 'base_dir' => "${base_dir}" }),
+    mode    => '0775',
+    owner   => 'root',
+    group   => 'root',
+  }
 }
 
 include author_dispatcher
