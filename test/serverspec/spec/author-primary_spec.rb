ed7bc58fc742708a6b4412beb4fe080f3af9fd3a
Cliffano Subagio <cliffano@gmail.com>
2017-03-27 02:51:30 +1100

    Add checks for https ports.

--- test/serverspec/spec/author-primary_spec.rb
+++ test/serverspec/spec/author-primary_spec.rb
@@ -1,7 +1,5 @@
 require_relative 'spec_helper'
 
-aem_port = '4502'
-
 # Service will be renamed to 'aem' on next puppet-aem release.
 # https://github.com/bstopp/puppet-aem/commit/a28d87fbf6bafc81ff00dec1759d8848708f32af
 describe service('aem-aem') do
@@ -9,6 +7,10 @@ describe service('aem-aem') do
   it { should be_running }
 end
 
-describe port(aem_port) do
+describe port(4502) do
+  it { should be_listening }
+end
+
+describe port(5433) do
   it { should be_listening }
 end
--- test/serverspec/spec/publish-dispatcher_spec.rb
+++ test/serverspec/spec/publish-dispatcher_spec.rb
@@ -8,3 +8,7 @@ end
 describe port(80) do
   it { should be_listening }
 end
+
+describe port(443) do
+  it { should be_listening }
+end
