b7a9074c3014ddf42b7e377a41ef4506081b79a7
Jesus Gutierrez <lenuhc@gmail.com>
2017-03-29 11:51:32 +1100

    Changed probability from .5 to 1

--- manifests/chaos-monkey.pp
+++ manifests/chaos-monkey.pp
@@ -6,31 +6,31 @@ class chaos_monkey (
 
   simianarmy::chaos_properties::asg { $::orchestratorasg:
     enabled                  => true,
-    probability              => '0.5',
+    probability              => '1.0',
     max_terminations_per_day => '1.0',
   }
   # Chaos Monkey
   simianarmy::chaos_properties::asg { $facts['aws:autoscaling:groupname']:
     enabled                  => true,
-    probability              => '0.5',
+    probability              => '1.0',
     max_terminations_per_day => '1.0',
   }
   # Publish
   simianarmy::chaos_properties::asg { $::publisherasg:
     enabled                  => true,
-    probability              => '0.5',
+    probability              => '1.0',
     max_terminations_per_day => '1.0',
   }
   # Publish-dispatcher
   simianarmy::chaos_properties::asg { $::publisherdispatcherasg:
     enabled                  => true,
-    probability              => '0.5',
+    probability              => '1.0',
     max_terminations_per_day => '1.0',
   }
   # Author-Dispatcher
   simianarmy::chaos_properties::asg { $::authordispatcherasg:
     enabled                  => true,
-    probability              => '0.5',
+    probability              => '1.0',
     max_terminations_per_day => '1.0',
   }
 
