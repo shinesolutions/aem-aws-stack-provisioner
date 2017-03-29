9e4ce54199192b44f7d2f3217ab02945def61a8c
Jesus Gutierrez <lenuhc@gmail.com>
2017-03-29 11:06:57 +1100

    Changed doubles to strings

--- manifests/chaos-monkey.pp
+++ manifests/chaos-monkey.pp
@@ -6,32 +6,32 @@ class chaos_monkey (
 
   simianarmy::chaos_properties::asg { $::orchestratorasg:
     enabled                  => true,
-    probability              => 0.5,
-    max_terminations_per_day => 1.0,
+    probability              => '0.5',
+    max_terminations_per_day => '1.0',
   }
   # Chaos Monkey
   simianarmy::chaos_properties::asg { $facts['aws:autoscaling:groupname']:
     enabled                  => true,
-    probability              => 0.5,
-    max_terminations_per_day => 1.0,
+    probability              => '0.5',
+    max_terminations_per_day => '1.0',
   }
   # Publish
   simianarmy::chaos_properties::asg { $::publisherasg:
     enabled                  => true,
-    probability              => 0.5,
-    max_terminations_per_day => 1.0,
+    probability              => '0.5',
+    max_terminations_per_day => '1.0',
   }
   # Publish-dispatcher
   simianarmy::chaos_properties::asg { $::publisherdispatcherasg:
     enabled                  => true,
-    probability              => 0.5,
-    max_terminations_per_day => 1.0,
+    probability              => '0.5',
+    max_terminations_per_day => '1.0',
   }
   # Author-Dispatcher
   simianarmy::chaos_properties::asg { $::authordispatcherasg:
     enabled                  => true,
-    probability              => 0.5,
-    max_terminations_per_day => 1.0,
+    probability              => '0.5',
+    max_terminations_per_day => '1.0',
   }
 
 }
