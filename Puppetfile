4047da333e5d842d51fb47f6a707331eb8f4c4a3
Jesus Gutierrez <lenuhc@gmail.com>
2017-03-30 12:06:42 +1100

    Added chaos monkey script (#18)
    * Added chaos monkey script
    
    * Made changes according to comments
    
    * Changed configuration for the five ASGs
    
    * Added ASG config
    
    * Added changes for Chaos Monkey
    
    * Changed doubles to strings
    
    * Changed probability from .5 to 1
    
    * Changed properties for Chaos Monkey
    
    * Added stack prefix to sdb domain
    
    * Added stack prefix to sdb domain in the correct properties
    
    * changed stack_prefix to stackprefix for sdb creation
    
    * Removed comments

--- Puppetfile
+++ Puppetfile
@@ -4,4 +4,4 @@ mod 'puppet/archive', '1.3.0'
 mod 'puppetlabs/java_ks', '1.5.0' # temporary until puppet-aem-resources is retrieved from Puppet Forge
 mod 'shinesolutions/aem_orchestrator', :git => 'https://github.com/shinesolutions/puppet-aem-orchestrator'
 mod 'shinesolutions/aem_resources', :git => 'https://github.com/shinesolutions/puppet-aem-resources'
-mod 'shinesolutions/simianarmy', :git => 'https://github.com/shinesolutions/puppet-simianarmy', :ref => '0.2.0'
+mod 'shinesolutions/simianarmy', :git => 'https://github.com/shinesolutions/puppet-simianarmy'
--- manifests/chaos-monkey.pp
+++ manifests/chaos-monkey.pp
@@ -4,6 +4,36 @@ class chaos_monkey (
 
   include simianarmy
 
+  simianarmy::chaos_properties::asg { $::orchestratorasg:
+    enabled                  => true,
+    probability              => '1.0',
+    max_terminations_per_day => '1.0',
+  }
+  # Chaos Monkey
+  simianarmy::chaos_properties::asg { $facts['aws:autoscaling:groupname']:
+    enabled                  => true,
+    probability              => '1.0',
+    max_terminations_per_day => '1.0',
+  }
+  # Publish
+  simianarmy::chaos_properties::asg { $::publisherasg:
+    enabled                  => true,
+    probability              => '1.0',
+    max_terminations_per_day => '1.0',
+  }
+  # Publish-dispatcher
+  simianarmy::chaos_properties::asg { $::publisherdispatcherasg:
+    enabled                  => true,
+    probability              => '1.0',
+    max_terminations_per_day => '1.0',
+  }
+  # Author-Dispatcher
+  simianarmy::chaos_properties::asg { $::authordispatcherasg:
+    enabled                  => true,
+    probability              => '1.0',
+    max_terminations_per_day => '1.0',
+  }
+
 }
 
 include chaos_monkey
