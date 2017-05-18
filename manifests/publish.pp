aae014509d7005cbc3276e06483cc1d44b1316cf
Michael Diender <michael.diender@shinesolutions.com>
2017-05-18 16:11:54 +1000

    increase the publish aem wait for login page time to 10 minutes. up from 5 minutes.

--- manifests/publish.pp
+++ manifests/publish.pp
@@ -72,7 +72,7 @@ class publish (
     enable => true,
   } -> aem_aem { 'Wait until login page is ready':
     ensure                     => login_page_is_ready,
-    retries_max_tries          => 60,
+    retries_max_tries          => 120,
     retries_base_sleep_seconds => 5,
     retries_max_sleep_seconds  => 5,
   } -> aem_bundle { 'Stop webdav bundle':
