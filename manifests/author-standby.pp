072913b75b5536bfa9d5f00551f54845e05c71cb
James Sinclair <james.sinclair@shinesolutions.com>
2017-05-19 13:28:29 +1000

    Define the Java memory MBeans we want collectd to report to CloudWatch.

--- manifests/author-standby.pp
+++ manifests/author-standby.pp
@@ -45,6 +45,53 @@ class author_standby (
   }
 
   collectd::plugin::genericjmx::mbean {
+    'garbage_collector':
+      object_name     => 'java.lang:type=GarbageCollector,*',
+      instance_prefix => 'gc-',
+      instance_from   => 'name',
+      values          => [
+        {
+          'type'    => 'invocations',
+          table     => false,
+          attribute => 'CollectionCount',
+        },
+        {
+          'type'          => 'total_time_in_ms',
+          instance_prefix => 'collection_time',
+          table           => false,
+          attribute       => 'CollectionTime',
+        },
+      ];
+    'memory-heap':
+      object_name     => 'java.lang:type=Memory',
+      instance_prefix => 'memory-heap',
+      values          => [
+        {
+          'type'    => 'jmx_memory',
+          table     => true,
+          attribute => 'HeapMemoryUsage',
+        },
+      ];
+    'memory-nonheap':
+      object_name     => 'java.lang:type=Memory',
+      instance_prefix => 'memory-nonheap',
+      values          => [
+        {
+          'type'    => 'jmx_memory',
+          table     => true,
+          attribute => 'NonHeapMemoryUsage',
+        },
+      ];
+    'memory-permgen':
+      object_name     => 'java.lang:type=MemoryPool,name=*Perm Gen',
+      instance_prefix => 'memory-permgen',
+      values          => [
+        {
+          'type'    => 'jmx_memory',
+          table     => true,
+          attribute => 'Usage',
+        },
+      ];
     'standby-status':
       object_name     => 'org.apache.jackrabbit.oak:*,name=Status,type=*Standby*',
       instance_prefix => "${::stackprefix}-standby-status",
