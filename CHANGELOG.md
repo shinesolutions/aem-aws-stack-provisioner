1f11f23f7b12c57f067672e50df7febb5e4f23a6
Michael Diender <michael.diender@shinesolutions.com>
2017-05-26 15:16:20 +1000

    make the offline snapshot time configurable.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ### 1.0.1
 * Add weekly offline compaction snapshot job to orchestrator
+* Make the offline snapshot time configurable
 
 ### 1.0.0
 * Initial version
--- manifests/orchestrator.pp
+++ manifests/orchestrator.pp
@@ -3,11 +3,16 @@ File {
 }
 
 class orchestrator (
-  $base_dir
+  $base_dir,
+  $offline_snapshot_hour = 1,
+  $offline_snapshot_minute = 15,
+  $enable_weekly_offline_compaction_snapshot = true,
 ) {
+
   Archive {
     checksum_verify => false,
   }
+
   include aem_orchestrator
 
   file { "${base_dir}/aem-tools/":
@@ -17,7 +22,7 @@ class orchestrator (
     group  => 'root',
   }
 
-  # daily (except sunday) offline-snapshot
+  # offline-snapshot
   file { "${base_dir}/aem-tools/stack-offline-snapshot-message.json":
     ensure  => present,
     content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/stack-offline-snapshot-message.json.epp", { 'stack_prefix' => "${::stackprefix}"}),
@@ -31,16 +36,38 @@ class orchestrator (
     mode    => '0775',
     owner   => 'root',
     group   => 'root',
-  } -> cron { 'nightly-stack-offline-snapshot':
-    command     => "cd ${base_dir}/aem-tools && ./stack-offline-snapshot.sh >/var/log/stack-offline-snapshot.log 2>&1",
-    user        => 'root',
-    hour        => 1,
-    minute      => 15,
-    weekday     => '1-6',
-    environment => ["PATH=${::cron_env_path}", "https_proxy=\"${::cron_https_proxy}\""],
   }
 
-  # weekly offline-compaction-snapshot
+  if $enable_weekly_offline_compaction_snapshot {
+
+    # Monday to Saturday
+    cron { 'nightly-stack-offline-snapshot':
+      command     => "cd ${base_dir}/aem-tools && ./stack-offline-snapshot.sh >/var/log/stack-offline-snapshot.log 2>&1",
+      user        => 'root',
+      hour        => $offline_snapshot_hour,
+      minute      => $offline_snapshot_minute,
+      weekday     => '1-6',
+      environment => ["PATH=${::cron_env_path}", "https_proxy=\"${::cron_https_proxy}\""],
+      require     => File["${base_dir}/aem-tools/stack-offline-snapshot.sh"],
+    }
+
+  }
+  else {
+
+    # Sunday to Saturday
+    cron { 'nightly-stack-offline-snapshot':
+      command     => "cd ${base_dir}/aem-tools && ./stack-offline-snapshot.sh >/var/log/stack-offline-snapshot.log 2>&1",
+      user        => 'root',
+      hour        => $offline_snapshot_hour,
+      minute      => $offline_snapshot_minute,
+      weekday     => '0-6',
+      environment => ["PATH=${::cron_env_path}", "https_proxy=\"${::cron_https_proxy}\""],
+      require     => File["${base_dir}/aem-tools/stack-offline-snapshot.sh"],
+    }
+
+  }
+
+  # stack offline-compaction-snapshot
   file { "${base_dir}/aem-tools/stack-offline-compaction-snapshot-message.json":
     ensure  => present,
     content => epp("${base_dir}/aem-aws-stack-provisioner/templates/aem-tools/stack-offline-compaction-snapshot-message.json.epp", { 'stack_prefix' => "${::stackprefix}"}),
@@ -54,13 +81,22 @@ class orchestrator (
     mode    => '0775',
     owner   => 'root',
     group   => 'root',
-  } -> cron { 'weekly-stack-offline-compaction-snapshot':
-    command     => "cd ${base_dir}/aem-tools && ./stack-offline-compaction-snapshot.sh >/var/log/stack-offline-compaction-snapshot.log 2>&1",
-    user        => 'root',
-    hour        => 6,
-    minute      => 15,
-    weekday     => 0,
-    environment => ["PATH=${::cron_env_path}", "https_proxy=\"${::cron_https_proxy}\""],
+  }
+
+  if $enable_weekly_offline_compaction_snapshot {
+
+    # Sunday only
+
+    cron { 'weekly-stack-offline-compaction-snapshot':
+      command     => "cd ${base_dir}/aem-tools && ./stack-offline-compaction-snapshot.sh >/var/log/stack-offline-compaction-snapshot.log 2>&1",
+      user        => 'root',
+      hour        => $offline_snapshot_hour,
+      minute      => $offline_snapshot_minute,
+      weekday     => 0,
+      environment => ["PATH=${::cron_env_path}", "https_proxy=\"${::cron_https_proxy}\""],
+      require     => File["${base_dir}/aem-tools/stack-offline-compaction-snapshot.sh"],
+    }
+
   }
 
 }
