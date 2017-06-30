08335b37150b5bb2f41a6a0463ea8479a205d2c9
Peter Fuggle <peter.fuggle@shinetech.com>
2017-06-30 16:03:34 +1000

    And on line 37 need --volume-type

--- manifests/publish.pp
+++ manifests/publish.pp
@@ -34,7 +34,7 @@ class publish (
       exec { "Attach volume from snapshot ID ${snapshotid}":
         cwd     => '/opt/shinesolutions/aws-tools/',
         path    => ["${base_dir}/aws-tools", '/usr/bin', '/opt/puppetlabs/bin/'],
-        command => "./snapshot_attach.py --device /dev/sdb --device-alias /dev/xvdb --snapshot-id ${snapshotid} -vvvv",
+        command => "./snapshot_attach.py --device /dev/sdb --device-alias /dev/xvdb --volume-type ${vol_type} --snapshot-id ${snapshotid} -vvvv",
         before  => Service['aem-aem'],
       }
 
