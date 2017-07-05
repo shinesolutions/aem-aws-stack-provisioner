e62cac3b8eca47c1ce789251d2a680ec70971967
Andy Wang <andy.wang@shinesolutions.com>
2017-07-05 14:07:22 +1000

    enable serverspec test for chaos monkey

--- test/serverspec/spec/chaos-monkey_spec.rb
+++ test/serverspec/spec/chaos-monkey_spec.rb
@@ -1,1 +1,10 @@
 require_relative 'spec_helper'
+
+
+tomcat_srv_name = @hiera.lookup('chaos_monkey::tomcat_srv_name', nil, nil)
+tomcat_srv_name ||= 'tomcat'
+
+describe service(tomcat_srv_name) do
+  it { should be_enabled }
+  it { should be_running }
+end
--- test/serverspec/spec/spec_helper.rb
+++ test/serverspec/spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'serverspec'
+require 'hiera'
+
 set :backend, :exec
+
+hiera_conf = File.expand_path(File.join(__FILE__, '../../../../conf/hiera.yaml'))
+@hiera = Hiera.new(:config => hiera_conf)
