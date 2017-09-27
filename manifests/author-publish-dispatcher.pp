e7e717d25943271f7c75da0278ae7d06e20f51bc
Stephen Shim <stephen.shim@shinesolutions.com>
2017-09-27 17:24:15 +1000

    Remove test class

--- manifests/author-publish-dispatcher.pp
+++ manifests/author-publish-dispatcher.pp
@@ -7,17 +7,9 @@ class author_publish_dispatcher (
   $tmp_dir,
 ) {
 
-  notify { 'It works!': }
-
-  # config_mytest (For testing only, remove when done)
-  #include aem_curator::config_mytest
-  #class { 'aem_curator::config_mytest':
-  #}
-
-  #include aem_curator::config_author_primary
+  include aem_curator::config_author_primary
   include aem_curator::config_publish
-  #include aem_curator::config_publish_dispatcher
-
+  include aem_curator::config_publish_dispatcher
 
 }
 
