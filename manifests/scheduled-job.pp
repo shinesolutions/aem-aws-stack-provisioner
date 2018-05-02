12bfcdc3b7041acfa47641c6fd135748f9790537
Michael Bloch <michael.bloch@shinesolutions.com>
2018-05-03 09:56:23 +1000

    Replaced hiera lookup with alias for proper boolean handling in manifest
    * Replaced hiera lookup with alias for proper boolean handling in manifest

--- manifests/scheduled-job.pp
+++ manifests/scheduled-job.pp
@@ -4,18 +4,18 @@ File {
 
 class config_scheduled_jobs (
   $base_dir,
-  $env_path                                  = $::cron_env_path,
-  $https_proxy                               = $::cron_https_proxy,
-  $cron_offline_compaction_snapshot_enable   = undef,
-  $cron_offline_snapshot_enable              = undef,
-  $cron_export_enable                        = undef,
-  $cron_live_snapshot_enable                 = undef,
-  $cron_export_hour                          = '2',
-  $cron_export_minute                        = '0',
-  $cron_export_weekday                       = '0-7',
-  $cron_live_snapshot_hour                   = '*',
-  $cron_live_snapshot_minute                 = '0',
-  $cron_live_snapshot_weekday                = '0-7',
+  $env_path                                       = $::cron_env_path,
+  $https_proxy                                    = $::cron_https_proxy,
+  $cron_offline_compaction_snapshot_enable        = false,
+  $cron_offline_snapshot_enable                   = false,
+  $cron_export_enable                             = false,
+  $cron_live_snapshot_enable                      = false,
+  $cron_export_hour                               = '2',
+  $cron_export_minute                             = '0',
+  $cron_export_weekday                            = '0-7',
+  $cron_live_snapshot_hour                        = '*',
+  $cron_live_snapshot_minute                      = '0',
+  $cron_live_snapshot_weekday                     = '0-7',
   $cron_offline_compaction_snapshot_hour          = '1',
   $cron_offline_compaction_snapshot_minute        = '15',
   $cron_offline_compaction_snapshot_weekday       = '1',
@@ -24,7 +24,7 @@ class config_scheduled_jobs (
   $cron_offline_snapshot_weekday                  = '2-7',
 ) {
 
-  if $cron_offline_snapshot_enable == 'true' {
+  if $cron_offline_snapshot_enable == true {
     cron { 'stack-offline-snapshot':
       ensure      => present,
       command     => "cd ${base_dir}/aem-tools && ./stack-offline-snapshot.sh >/var/log/stack-offline-snapshot.log 2>&1",
@@ -43,7 +43,7 @@ class config_scheduled_jobs (
     }
   }
 
-  if $cron_offline_compaction_snapshot_enable == 'true' {
+  if $cron_offline_compaction_snapshot_enable == true {
     cron { 'stack-offline-compaction-snapshot':
       ensure      => present,
       command     => "cd ${base_dir}/aem-tools && ./stack-offline-compaction-snapshot.sh >/var/log/stack-offline-compaction-snapshot.log 2>&1",
@@ -62,7 +62,7 @@ class config_scheduled_jobs (
     }
   }
 
-  if $cron_live_snapshot_enable == 'true' {
+  if $cron_live_snapshot_enable == true {
     cron { 'live-snapshot-backup':
       ensure      => present,
       command     => "${base_dir}/aem-tools/live-snapshot-backup.sh >>/var/log/live-snapshot-backup.log 2>&1",
@@ -81,7 +81,7 @@ class config_scheduled_jobs (
     }
   }
 
-  if $cron_export_enable == 'true' {
+  if $cron_export_enable == true {
     cron { 'export-backups':
       ensure      => present,
       command     => "${base_dir}/aem-tools/export-backups.sh export-backups-descriptor.json >>/var/log/export-backups.log 2>&1",
