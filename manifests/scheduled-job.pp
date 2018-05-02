1e63ba39f36a70673c413dc45733869d1d901f36
Michael Bloch <michael.bloch@shinesolutions.com>
2018-05-02 11:02:54 +1000

    Updated orchestrator data cronjobs parameter names

--- manifests/scheduled-job.pp
+++ manifests/scheduled-job.pp
@@ -6,8 +6,8 @@ class config_scheduled_jobs (
   $base_dir,
   $env_path                                  = $::cron_env_path,
   $https_proxy                               = $::cron_https_proxy,
-  $enable_offline_compaction_snapshot_cron   = undef,
-  $enable_offline_snapshot_cron              = undef,
+  $cron_offline_compaction_snapshot_enable   = undef,
+  $cron_offline_snapshot_enable              = undef,
   $cron_export_enable                        = undef,
   $cron_live_snapshot_enable                 = undef,
   $cron_export_hour                          = '2',
@@ -16,22 +16,22 @@ class config_scheduled_jobs (
   $cron_live_snapshot_hour                   = '*',
   $cron_live_snapshot_minute                 = '0',
   $cron_live_snapshot_weekday                = '0-7',
-  $offline_compaction_snapshot_hour          = '1',
-  $offline_compaction_snapshot_minute        = '15',
-  $offline_compaction_snapshot_weekday       = '1',
-  $offline_snapshot_hour                     = '1',
-  $offline_snapshot_minute                   = '15',
-  $offline_snapshot_weekday                  = '2-7',
+  $cron_offline_compaction_snapshot_hour          = '1',
+  $cron_offline_compaction_snapshot_minute        = '15',
+  $cron_offline_compaction_snapshot_weekday       = '1',
+  $cron_offline_snapshot_hour                     = '1',
+  $cron_offline_snapshot_minute                   = '15',
+  $cron_offline_snapshot_weekday                  = '2-7',
 ) {
 
-  if $enable_offline_snapshot_cron == 'true' {
+  if $cron_offline_snapshot_enable == 'true' {
     cron { 'stack-offline-snapshot':
       ensure      => present,
       command     => "cd ${base_dir}/aem-tools && ./stack-offline-snapshot.sh >/var/log/stack-offline-snapshot.log 2>&1",
       user        => 'root',
-      hour        => $offline_snapshot_hour,
-      minute      => $offline_snapshot_minute,
-      weekday     => $offline_snapshot_weekday,
+      hour        => $cron_offline_snapshot_hour,
+      minute      => $cron_offline_snapshot_minute,
+      weekday     => $cron_offline_snapshot_weekday,
       environment => ["PATH=${env_path}", "https_proxy=\"${https_proxy}\""],
     }
   } else {
@@ -43,14 +43,14 @@ class config_scheduled_jobs (
     }
   }
 
-  if $enable_offline_compaction_snapshot_cron == 'true' {
+  if $cron_offline_compaction_snapshot_enable == 'true' {
     cron { 'stack-offline-compaction-snapshot':
       ensure      => present,
       command     => "cd ${base_dir}/aem-tools && ./stack-offline-compaction-snapshot.sh >/var/log/stack-offline-compaction-snapshot.log 2>&1",
       user        => 'root',
-      hour        => $offline_compaction_snapshot_hour,
-      minute      => $offline_compaction_snapshot_minute,
-      weekday     => $offline_compaction_snapshot_weekday,
+      hour        => $cron_offline_compaction_snapshot_hour,
+      minute      => $cron_offline_compaction_snapshot_minute,
+      weekday     => $cron_offline_compaction_snapshot_weekday,
       environment => ["PATH=${env_path}", "https_proxy=\"${https_proxy}\""],
     }
   } else {
