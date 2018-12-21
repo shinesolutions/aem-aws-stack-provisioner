b7885c9ab6246aa0a5ff85e4359019bdab35cbe3
Michael Diender <michael.diender@shinesolutions.com>
2018-12-21 14:38:34 +1100

    use systemd_service when os is amazon linux 2

--- test/inspec/author-publish-dispatcher_spec.rb
+++ test/inspec/author-publish-dispatcher_spec.rb
@@ -1,8 +1,26 @@
 require_relative './spec_helper'
 
-describe service('aem-author') do
-  it { should be_enabled }
-  it { should be_running }
+
+# inspec version 1.51.6. doesn't support Amazon Linux 2. It assumes it uses Upstart.
+# inspec version is locked to 1.51.6 to use train version 0.32 because it doesn't have an aws-sdk dependency:
+# https://github.com/inspec/inspec/blob/v1.51.6/inspec.gemspec#L29
+# inspec supports amazon linux 2 from version v2.1.30 (2018-04-05)
+# https://github.com/inspec/inspec/blob/master/CHANGELOG.md#v2130-2018-04-05
+# remove this bespoke handling once upgraded.
+if %w[amazon].include?(os[:name]) && !os[:release].start_with?('20\d\d')
+
+  describe systemd_service('aem-author') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+else
+
+  describe service('aem-author') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
 end
 
 describe port(4502) do
@@ -21,9 +39,20 @@ describe ssl(port: 5432) do
   it { should be_enabled }
 end
 
-describe service('aem-publish') do
-  it { should be_enabled }
-  it { should be_running }
+if %w[amazon].include?(os[:name]) && !os[:release].start_with?('20\d\d')
+
+  describe systemd_service('aem-publish') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+else
+
+  describe service('aem-publish') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
 end
 
 describe port(4503) do
@@ -42,9 +71,20 @@ describe ssl(port: 5433) do
   it { should be_enabled }
 end
 
-describe service('httpd') do
-  it { should be_enabled }
-  it { should be_running }
+if %w[amazon].include?(os[:name]) && !os[:release].start_with?('20\d\d')
+
+  describe systemd_service('httpd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+else
+
+  describe service('httpd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
 end
 
 describe port(80) do
